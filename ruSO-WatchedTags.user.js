f988c9b5687ec80e2393ad52bceb7e86246fdfc9
XelaNimed <xela.nimed@gmail.com>
2019-05-02 11:56:39 +0200

    Update ruSO-WatchedTags.user.js
    - has added CSS classes to hyperlink;
    - hide eye icon in tag block header;

--- ruSO-WatchedTags.user.js
+++ ruSO-WatchedTags.user.js
@@ -13,7 +13,8 @@ window.addEventListener('load', function() {
 
     var tags = [],
         urlPrefix = window.location.origin + "/questions/tagged/",
-        spanText = "Отслеживаемые метки";
+        spanText = "Отслеживаемые метки",
+        $sidebar = $("#sidebar");
 
     $(".js-watched-tag-list a.user-tag").each(function (idx, itm) {
         var url = itm.href;
@@ -23,8 +24,9 @@ window.addEventListener('load', function() {
     if (tags.length) {
         var url = urlPrefix + tags.join("+or+");
         var spanArr = $("#sidebar").find("span:contains('" + spanText + "')");
+        $sidebar.find("span.grid--cell.mr4").hide();
         if(spanArr.length > 0) {
-            spanArr[0].innerHTML = '<a href="' + url + '">' + spanText + '</a>';
+            spanArr[0].innerHTML = '<a class="post-tag user-tag" href="' + url + '">' + spanText + '</a>';
         }
     }
 
