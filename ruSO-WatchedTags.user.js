64d93ed9921c528e00fbb9e172825559177dceeb
XelaNimed <xela.nimed@gmail.com>
2019-05-17 00:37:49 +0200

    Visibility state now stored in the local storage
    The visibility state of the element with important posts on Meta is now stored in the local storage. 
    Small improvements and fixes.

--- ruSO-WatchedTags.user.js
+++ ruSO-WatchedTags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ruSO-WatchedTags
 // @namespace    https://github.com/XelaNimed
-// @version      0.3
+// @version      0.4
 // @description  Various improvements for Russian-language StackOverflow.
 // @author       Xela Nimed
 // @match        https://ru.stackoverflow.com/*
@@ -11,18 +11,24 @@
 // ==/UserScript==
 window.addEventListener('load', function() {
 
+    initLocalStorage();
+
     var tags = [],
         urlPrefix = window.location.origin + "/questions/tagged/",
-        spanText = "Отслеживаемые метки",
-        $sidebar = $("#sidebar");
+        watchedTagsText = "Отслеживаемые метки",
+        importantOnMetaText = "Важное на Мете",
+        $sidebar = $("#sidebar"),
+        $impMeta = $sidebar.find("div.s-sidebarwidget--header:contains('" + importantOnMetaText + "')");
 
-    $sidebar
-    .find("div.s-sidebarwidget--header:contains('Важное на Мете')")
+    $impMeta
     .attr("title", "Click to toggle")
     .css("cursor", "pointer")
-    .on("click", function(e){
-        $(this).parent().find("ul.s-sidebarwidget--content, div:not(:first)").toggle();
+    .on("click", function() {
+        let isVisible = localStorage.getItem("showImportantMetaPosts") === "true";
+        localStorage.setItem("showImportantMetaPosts", !isVisible);
+        showHideMetaPosts($impMeta);
     });
+    showHideMetaPosts($impMeta);
 
     $(".js-watched-tag-list a.user-tag").each(function (idx, itm) {
         var url = itm.href;
@@ -30,12 +36,21 @@ window.addEventListener('load', function() {
     });
 
     if (tags.length) {
-        var url = urlPrefix + tags.join("+or+");
-        var spanArr = $sidebar.find("span:contains('" + spanText + "')");
+        let url = urlPrefix + tags.join("+or+");
+        let spanArr = $sidebar.find("span:contains('" + watchedTagsText + "')");
         $sidebar.find("span.grid--cell.mr4").hide();
         if(spanArr.length > 0) {
-            spanArr[0].innerHTML = '<a class="post-tag user-tag" href="' + url + '">' + spanText + '</a>';
+            spanArr[0].innerHTML = '<a class="post-tag user-tag" href="' + url + '">' + watchedTagsText + '</a>';
         }
     }
 
 }, false);
+
+function initLocalStorage() {
+    localStorage.getItem("showImportantMetaPosts") || localStorage.setItem("showImportantMetaPosts", true);
+}
+
+function showHideMetaPosts($elem) {
+    let isVisible = localStorage.getItem("showImportantMetaPosts") === "true";
+    $elem.parent().find("ul.s-sidebarwidget--content, div:not(:first)")[isVisible ? "show" : "hide"]();
+}
