68abf81e0944fa205ab7a948a2097bf62f61e146
XelaNimed <xela.nimed@gmail.com>
2020-01-31 17:02:51 +0100

    Update stackoverflow.user.js
    Minor improvements and bug fixes.

--- stackoverflow.user.js
+++ stackoverflow.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        SO
 // @namespace   https://github.com/XelaNimed
-// @version     0.7.0
+// @version     0.7.1
 // @description Various improvements for StackOverflow.
-// @author      Xela Nimed
+// @author      XelaNimed
 // @match       https://*.stackoverflow.com/*
 // @match       https://*.meta.stackoverflow.com/*
 // @grant       none
@@ -20,44 +20,45 @@ window.addEventListener('load', function () {
 }, false);
 
 var ruSO = {
-	$sidebar: $("#sidebar"),
-	$content: $("#content"),
-	$container: $("body>.container"),
+	$sidebar: $('#sidebar'),
+	$content: $('#content'),
+	$container: $('body>.container'),
+    $fullWidthBtn: null,
 	params: {
 		animationSpeed: 250
 	},
 	keys: {
-		showMetasKey: "showMetaPosts",
-        contentMaxWidth: "contentMaxWidth",
-        containerMaxWidth: "containerMaxWidth",
-        isFullWidth: "isFullWidth"
+		showMetasKey: 'showMetaPosts',
+        contentMaxWidth: 'contentMaxWidth',
+        containerMaxWidth: 'containerMaxWidth',
+        fooFullWidth: 'fooFullWidth'
 	},
 	strings: {
-		watchedTagsText: "Отслеживаемые метки",
-		clickToToggle: "Скрыть/показать",
-		setFullWidth: "Растянуть",
-		resetFullWidth: "Восстановить"
+		watchedTagsText: 'Отслеживаемые метки',
+		clickToToggle: 'Скрыть/показать',
+		setFullWidth: 'Растянуть',
+		resetFullWidth: 'Восстановить'
 	},
 	initLocalStorage: function initLocalStorage() {
-		localStorage.getItem(this.keys.showMetasKey) || localStorage.setItem(this.keys.showMetasKey, true);
-        localStorage.setItem(this.keys.containerMaxWidth, this.$container.css('max-width'));
-        localStorage.setItem(this.keys.contentMaxWidth, this.$content.css('max-width'));
-        localStorage.setItem(this.keys.isFullWidth, false);
+		localStorage[this.keys.showMetasKey] || localStorage.setItem(this.keys.showMetasKey, true);
+        localStorage[this.keys.containerMaxWidth] = this.$container.css('max-width');
+        localStorage[this.keys.contentMaxWidth] = this.$content.css('max-width');
+        localStorage[this.keys.fooFullWidth] = 'setFullWidth';
 		return this;
 	},
 	addButtons: function () {
 		var self = this,
 		addWatchedTags = function () {
 			var tags = [],
-			urlPrefix = window.location.origin + "/questions/tagged/";
-			$(".js-watched-tag-list a.user-tag").each(function (idx, itm) {
+			urlPrefix = window.location.origin + '/questions/tagged/';
+			$('.js-watched-tag-list a.user-tag').each(function (idx, itm) {
 				let url = itm.href;
-				tags.push(url.substring(url.lastIndexOf("/") + 1));
+				tags.push(url.substring(url.lastIndexOf('/') + 1));
 			});
 			if (tags.length) {
-				let url = urlPrefix + tags.join("+or+");
+				let url = urlPrefix + tags.join('+or+');
 				let spanArr = self.$sidebar.find("span:contains('" + self.strings.watchedTagsText + "')");
-				self.$sidebar.find("span.grid--cell.mr4").hide();
+				self.$sidebar.find('span.grid--cell.mr4').hide();
 				if (spanArr.length > 0) {
 					spanArr[0].innerHTML = '<a class="post-tag user-tag" href="' + url + '">' + self.strings.watchedTagsText + '</a>';
 				}
@@ -65,18 +66,18 @@ var ruSO = {
 		},
 		addMetaToggles = function () {
 			let showHideMetas = function ($elem) {
-				let isVisible = localStorage.getItem(self.keys.showMetasKey) === "true";
-				$elem.parent().find("ul.s-sidebarwidget--content")[isVisible ? "show" : "hide"](ruSO.params.animationSpeed);
+				let isVisible = localStorage.getItem(self.keys.showMetasKey) === 'true';
+				$elem.parent().find('ul.s-sidebarwidget--content')[isVisible ? 'show' : 'hide'](ruSO.params.animationSpeed);
 			};
 			self.$sidebar
-			.find("div.s-sidebarwidget:first div.s-sidebarwidget--header, #how-to-format, #how-to-title")
+			.find('div.s-sidebarwidget:first div.s-sidebarwidget--header, #how-to-format, #how-to-title')
 			.each(function (idx, itm) {
 				var $itm = $(itm);
 				$itm
-				.attr("title", ruSO.strings.clickToToggle)
-				.css("cursor", "pointer")
-				.on("click", function (e) {
-					let isVisible = localStorage.getItem(self.keys.showMetasKey) === "true";
+				.attr('title', ruSO.strings.clickToToggle)
+				.css('cursor', 'pointer')
+				.on('click', function (e) {
+					let isVisible = localStorage.getItem(self.keys.showMetasKey) === 'true';
 					localStorage.setItem(self.keys.showMetasKey, !isVisible);
 					showHideMetas($(e.target));
 				});
@@ -84,41 +85,34 @@ var ruSO = {
 			});
 		},
         addFullWidth = function() {
-            var $header = $("#question-header");
-            var $btn = $header.find('div').clone();
-            $btn.attr('id', 'set-full-width-btn').find('a')
+            var $header = $('#question-header');
+            self.$fullWidthBtn = $header.find('div').clone();
+            self.$fullWidthBtn.attr('id', 'set-full-width-btn').find('a')
             .removeClass('s-btn__primary')
             .addClass('s-btn__filled')
             .attr('href', '#')
             .text(self.strings.setFullWidth)
             .on('click', function() {
-                var isFullWidth = localStorage.getItem(self.keys.isFullWidth) === 'true';
-                if(isFullWidth) {
-                    self.resetFullWidth();
-                }
-                else {
-                    self.setFullWidth();
-                }
+                self[localStorage[self.keys.fooFullWidth]]();
             });
-            $header.append($btn);
+            $header.append(self.$fullWidthBtn);
         };
 		addWatchedTags();
 		addMetaToggles();
         addFullWidth();
 		return this;
 	},
-    setFullWidth: function(){
-        this.$container.css({'max-width':'none'});
-        this.$content.css({'max-width':'none'});
-        $('#set-full-width-btn').find('a').text(this.strings.resetFullWidth);
-        localStorage.setItem(this.keys.isFullWidth, true);
+    setFullWidth: function() {
+        this.$container.add(this.$content).css({'max-width':'none'});
+        this.$fullWidthBtn.find('a').text(this.strings.resetFullWidth);
+        localStorage[this.keys.fooFullWidth] = 'resetFullWidth';
         return this;
     },
-    resetFullWidth: function(){
-        this.$container.css({'max-width':localStorage.getItem(this.keys.containerMaxWidth)});
-        this.$content.css({'max-width':localStorage.getItem(this.keys.contentMaxWidth)});
-        $('#set-full-width-btn').find('a').text(this.strings.setFullWidth);
-        localStorage.setItem(this.keys.isFullWidth, false);
+    resetFullWidth: function() {
+        this.$container.css({'max-width': localStorage[this.keys.containerMaxWidth]});
+        this.$content.css({'max-width': localStorage[this.keys.contentMaxWidth]});
+        this.$fullWidthBtn.find('a').text(this.strings.setFullWidth);
+        localStorage[this.keys.fooFullWidth] = 'setFullWidth';
         return this;
     }
 };
